c3e2c1913a4a5def66983825acdb48f35083eccf
Maris Stefan Radu <marisstefan97@gmail.com>
2025-08-11 11:19:53 +0300

    Saved to make some conflicts

--- main.py
+++ main.py
@@ -4,20 +4,31 @@
 # afișați produsele și valoarea totală a inventarului.
 
 from product_manager import *
+#from Cart import *
+from random import *
+
 
 def main():
     our_list = ProductManager()
 
-    our_list.add_product(Product('Masa',130,20))
-    our_list.add_product(Product('Tv',4000,15))
-    our_list.add_product(Product('Scaun',30,20))
-    our_list.add_product(Product('Laptop',3750,15))
+    our_list.add_product(Product('Ceaun',130,20))
+    our_list.add_product(Product('Ecran',4000,15))
+    our_list.add_product(Product('Taburel',30,20))
+    our_list.add_product(Product('Mac',3750,15))
 
-    our_list.display_info()
-    print(f'Inventory total cost : {our_list.total()} Ron')
+    # our_list.display_info()
+    # print(f'Inventory total cost : {our_list.total()} Ron')
+    # our_list.remove('Scaun')
+    # our_list.display_info()
 
-    our_list.remove('Scaun')
-    our_list.display_info()
+    # customer_one = Cart()
+    # while len(customer_one.cart_items) < 3 :
+    #     for i in range(0,3):
+    #         customer_one.add_item_in_cart(choice(our_list.products))
+    
+    # customer_one.display_info()
+    # print(customer_one.total_cart())
+
 
 
 if __name__ == '__main__':
