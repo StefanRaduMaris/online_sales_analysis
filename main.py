1e8564c1f5f7b972a80cbde702ff6158ef3232ab
Maris Stefan Radu <marisstefan97@gmail.com>
2025-08-11 11:26:37 +0300

    Merge with class Cart

--- main.py
+++ main.py
@@ -4,6 +4,9 @@
 # afișați produsele și valoarea totală a inventarului.
 
 from product_manager import *
+#from Cart import *
+from random import *
+
 
 def main():
     our_list = ProductManager()
@@ -13,11 +16,20 @@ def main():
     our_list.add_product(Product('Taburel',30,20))
     our_list.add_product(Product('Mac',3750,15))
 
-    our_list.display_info()
-    print(f'Inventory total cost : {our_list.total()} Ron')
+    # our_list.display_info()
+    # print(f'Inventory total cost : {our_list.total()} Ron')
+    # our_list.remove('Scaun')
+    # our_list.display_info()
 
-    our_list.remove('Scaun')
-    our_list.display_info()
+    # customer_one = Cart()
+    # while len(customer_one.cart_items) < 3 :
+    #     for i in range(0,3):
+    #         customer_one.add_item_in_cart(choice(our_list.products))
+    
+    # customer_one.display_info()
+    # print(customer_one.total_cart())
+
+
 
 
 if __name__ == '__main__':
